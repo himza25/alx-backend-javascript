5bc9cac465fd883671e5f8cb283998ee52360cf1
himza25 <himza.of.theking95@gmail.com>
2024-04-08 19:34:03 -0700

    task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
-// Use `const` for variables that won't be reassigned
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Keep this function as is, since it doesn't involve variable declarations that need to be modified
 export function getLast() {
   return ' is okay';
 }
 
-// Use `let` for variables that might be reassigned
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
