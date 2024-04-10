1022c8bcf7585374723a048dbe135827f4903105
himza25 <himza.of.theking95@gmail.com>
2024-04-10 15:30:20 -0700

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,11 @@
+// 0-promise.js
+
+/**
+ * Returns a Promise without an explicit resolution value.
+ * @returns {Promise} A Promise object.
+ */
 export default function getResponseFromAPI() {
-  return new Promise((r) => {
-    r();
+  return new Promise((resolve) => {
+    resolve();
   });
 }
