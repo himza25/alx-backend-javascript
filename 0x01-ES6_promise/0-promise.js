a8ddcb9de4df8c257293fa92f6b96c673ecae7a0
himza25 <himza.of.theking95@gmail.com>
2024-04-10 15:20:53 -0700

    task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,14 @@
+/**
+ * Returns a Promise.
+ * @returns {Promise} A Promise object.
+ */
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Promise body
+    // You can resolve or reject based on your condition
+    // For the purpose of this task, the actual condition is not specified
+  });
+}
+
+// Exporting the function to be used in other files
+export default getResponseFromAPI;
