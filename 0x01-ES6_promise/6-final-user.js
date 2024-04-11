02143412dbc3c4809f2d387c55b85121aebd3857
himza25 <himza.of.theking95@gmail.com>
2024-04-10 19:06:39 -0700

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((args) => [
-    args.forEach((element) => ({
-      status: element.status,
-      value: element.status === 'fulfilled' ? element.value : element.reason,
-    })),
-  ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName).then((value) => ({
+    status: 'fulfilled',
+    value,
+  }));
+  const photo = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: `${error.name}: ${error.message}`,
+  }));
+  return [user, photo];
 }
