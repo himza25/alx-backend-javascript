671db552d179cc54f0915672dfebab2a7ae6583e
himza25 <himza.of.theking95@gmail.com>
2024-04-10 18:57:56 -0700

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+// 6-final-user.js
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.reason || result.value,
+    })));
+}
