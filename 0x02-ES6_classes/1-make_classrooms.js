a65dd2a344a120e6c31ee439a608e25f1ddb50ca
himza25 <himza.of.theking95@gmail.com>
2024-04-11 12:31:36 -0700

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,9 @@
-// 1-make_classrooms.js
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
-}
+const initializeRooms = () => [
+  new ClassRoom(19),
+  new ClassRoom(20),
+  new ClassRoom(34),
+];
 
 export default initializeRooms;
