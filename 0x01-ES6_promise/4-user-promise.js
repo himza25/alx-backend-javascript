ed1ed5faca76b90811f06b87b24cc1685691a7bf
himza25 <himza.of.theking95@gmail.com>
2024-04-10 18:22:57 -0700

    task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,7 @@
+// 4-user-promise.js
+
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({ firstName, lastName });
+  return Promise.resolve({ firstName, lastName });
 }
 
 export default signUpUser;
