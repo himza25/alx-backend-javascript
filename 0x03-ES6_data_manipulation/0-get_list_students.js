f1413fd077d3c14a5e840c4afbb92514a42b4ab8
himza25 <himza.of.theking95@gmail.com>
2024-04-16 08:01:05 -0700

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,12 +4,10 @@
  * Returns an array of student objects with their details
  * @returns {Object[]} Array of student objects
  */
-function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 }
-
-export default getListStudents;
