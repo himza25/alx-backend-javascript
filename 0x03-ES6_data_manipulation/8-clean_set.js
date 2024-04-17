4a857d381e9ed246d1b64a71cac5a1b024f940ae
himza25 <himza.of.theking95@gmail.com>
2024-04-16 18:31:32 -0700

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [];
+  for (const item of set) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
+    }
+  }
+  return result.join('-');
+}
+
+export default cleanSet;
