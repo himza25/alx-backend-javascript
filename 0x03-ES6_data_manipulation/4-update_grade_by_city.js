7ad6ee36e6b203eb5878fc62eaaacc42a3dc1c60
himza25 <himza.of.theking95@gmail.com>
2024-04-16 17:41:55 -0700

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location.localeCompare(city) === 0)
-    .map(student => {
+    .filter((student) => student.location.localeCompare(city) === 0)
+    .map((student) => {
       const gradeEntry = newGrades.find(grade => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: gradeEntry ? gradeEntry.grade : 'N/A'
-      };
+      student.grade = gradeEntry ? gradeEntry.grade : 'N/A';
+      return student;
     });
 }
