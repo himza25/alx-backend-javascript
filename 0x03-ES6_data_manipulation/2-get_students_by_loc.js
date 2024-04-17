fdafdfa3f96eb1ce985a31c4ee1709e454297fc0
himza25 <himza.of.theking95@gmail.com>
2024-04-16 17:09:15 -0700

    task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
-export default function getStudentsByLocation(students, city) {
-  return students.filter(student => student.location === city);
+export default function getStudentsByLocation(arrayOfObjects, location) {
+  if (!Array.isArray(arrayOfObjects)) {
+    return [];
+  }
+  return arrayOfObjects.filter((item) => item.location === location);
 }
