67d25f53a9f3efe6c75ae37dd893cb9142fdd1fc
himza25 <himza.of.theking95@gmail.com>
2024-06-04 12:50:07 -0700

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const countStudents = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter(line => line.trim() !== '');
+
+      const numberOfStudents = students.length;
+      console.log(`Number of students: ${numberOfStudents}`);
+
+      const fields = {};
+
+      students.forEach((student) => {
+        const [firstname, lastname, age, field] = student.split(',');
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+
+      for (const field in fields) {
+        if (Object.hasOwnProperty.call(fields, field)) {
+          const list = fields[field].join(', ');
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+        }
+      }
+
+      resolve();
+    });
+  });
+};
+
+module.exports = countStudents;
+
