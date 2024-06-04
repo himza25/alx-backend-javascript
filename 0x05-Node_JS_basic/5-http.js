84298e73858cb784b5b10c940bf1f3127ab1ced5
himza25 <himza.of.theking95@gmail.com>
2024-06-04 14:11:52 -0700

    task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url, true).pathname;
+
+  if (reqUrl === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (reqUrl === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents(database)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
