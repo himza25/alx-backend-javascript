9cc48dd1295d16c04de1cd35ea2f1e260ec2a19a
himza25 <himza.of.theking95@gmail.com>
2024-06-06 11:05:10 -0700

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,21 +1,20 @@
-const { assert } = require('chai');
+function calculateNumber(type, a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
 
-const calculateNumber = (type, a, b) => {
-  switch (type) {
-    case 'SUM':
-      return Math.round(a) + Math.round(b);
-    case 'SUBTRACT':
-      return Math.round(a) - Math.round(b);
-    case 'DIVIDE':
-      try {
-        assert(Math.round(b) !== 0);
-        return Math.round(a) / Math.round(b);
-      } catch (e) {
-        return 'Error';
-      }
-    default:
-      return 'Error';
-  }
-};
+    switch (type) {
+        case 'SUM':
+            return roundedA + roundedB;
+        case 'SUBTRACT':
+            return roundedA - roundedB;
+        case 'DIVIDE':
+            if (roundedB === 0) {
+                return 'Error';
+            }
+            return roundedA / roundedB;
+        default:
+            throw new Error('Invalid operation type');
+    }
+}
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,52 +1,38 @@
-const calculateNumber = require('./2-calcul_chai');
 const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-/* eslint-disable */
-describe('calculateNumber(SUM)', () => {
-  it('normal number', () => {
-    expect(calculateNumber('SUM', 1, 3)).equal(4);
-  });
-  it('zahlen number', () => {
-    expect(calculateNumber('SUM', 1, -3)).equal(-2);
-  });
-  it('radical number', () => {
-    expect(calculateNumber('SUM', 1, 3.7)).equal(5);
-    expect(calculateNumber('SUM', 1.2, 3.7)).equal(5);
-    expect(calculateNumber('SUM', 1.5, 3.7)).equal(6);
-  });
-  it('non-radical number', () => {
-    expect(calculateNumber('SUM', Math.E, Math.PI)).equal(6);
-  });
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+        it('should return 6 when inputs are 1.4 and 4.5', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+
+        it('should return 0 when inputs are -1.4 and 1.4', () => {
+            expect(calculateNumber('SUM', -1.4, 1.4)).to.equal(0);
+        });
+    });
+
+    describe('SUBTRACT', () => {
+        it('should return -4 when inputs are 1.4 and 4.5', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+
+        it('should return -2 when inputs are -1.4 and 1.4', () => {
+            expect(calculateNumber('SUBTRACT', -1.4, 1.4)).to.equal(-2);
+        });
+    });
+
+    describe('DIVIDE', () => {
+        it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.closeTo(0.2, 0.0001);
+        });
+
+        it('should return Error when dividing by 0', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+
+        it('should return -0.3333333333333333 when inputs are -1.4 and 2.7', () => {
+            expect(calculateNumber('DIVIDE', -1.4, 2.7)).to.be.closeTo(-0.3333333333333333, 0.0001);
+        });
+    });
 });
-describe('calculateNumber(SUBTRACT)', () => {
-  it('normal number', () => {
-    expect(calculateNumber('SUBTRACT', 1, 3)).equal(-2);
-  });
-  it('zahlen number', () => {
-    expect(calculateNumber('SUBTRACT', 1, -3)).equal(4);
-  });
-  it('radical number', () => {
-    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).equal(-2);
-  });
-  it('non-radical number', () => {
-    expect(calculateNumber('SUBTRACT', Math.E, Math.PI)).equal(0);
-  });
-});
-describe('calculateNumber(DIVIDE)', () => {
-  it('normal number', () => {
-    expect(calculateNumber('DIVIDE', 1, 3)).equal(1 / 3);
-  });
-  it('zahlen number', () => {
-    expect(calculateNumber('DIVIDE', 1, -3)).equal(-1 / 3);
-  });
-  it('radical number', () => {
-    expect(calculateNumber('DIVIDE', 1.5, 3.7)).equal(0.5);
-  });
-  it('non-radical number', () => {
-    expect(calculateNumber('DIVIDE', Math.E, Math.PI)).equal(1);
-  });
-  it('Infinity number', () => {
-    expect(calculateNumber('DIVIDE', 1, 0)).equal('Error');
-  });
-});
-/* eslint-enable */
