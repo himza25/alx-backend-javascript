7869468dfb5a4bef219ee1462f68c686578aa3d2
himza25 <himza.of.theking95@gmail.com>
2024-06-06 12:12:10 -0700

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+    it('should return a resolved promise with the correct data when success is true', (done) => {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                expect(response).to.be.an('object');
+                expect(response.data).to.equal('Successful response from the API');
+                done();
+            })
+            .catch(done);
+    });
+});
